02e253d3f07b9155e7dcf4a536565b183f014fbc
SOOKYUNG YOO <80819675+yoo-soo@users.noreply.github.com>
2021-10-14 23:32:48 +0900

    게임 난이도 수정
    0초~14초 일 때, 발판 길이(블록) 9~10개 랜덤 생성, 블럭 높이 0, 구멍 크기(블록) 1~2개 
    15초~29초일 때, 발판 길이(블록) 8~9개 랜덤 생성, 블럭 높이 -1~1, 구멍 크기(블록) 1~2개
    30초~44초 일 때, 발판 길이(블록) 7~8개 랜덤 생성, 블럭 높이 -2~2, 구멍 크기(블록) 2~3개
    45초~59초일 때, 발판 길이(블록) 5~6개 랜덤 생성, 블럭 높이 -2~2 구멍 크기(블록) 2~3개  
    60초~74초 일 때, 발판 길이(블록) 6~7개 랜덤 생성, 블럭 높이 -1~1, 구멍 크기(블록) 1~2개
    75초 이상일 때, 처음부터 15초마다 반복 ( 75초 이상일 때, 시간 0초로 초기화 )

--- RunningJumpGame/level_control.cs
+++ RunningJumpGame/level_control.cs
@@ -48,19 +48,111 @@ public class level_control : MonoBehaviour
     {
         switch (previous.block_type)
         {
-            case Block.TYPE.FLOOR:
-                current.block_type = Block.TYPE.HOLE;
-                current.max_count = 5;
-                current.height = previous.height;
-                break;
+            case Block.TYPE.FLOOR: // 블록
+                if (player_control.playTime / 15 >= 0.0f && player_control.playTime / 15 < 2.0f) // playtime이 0초~14초일 때
+                {
+                    current.block_type = Block.TYPE.HOLE;
+                    current.max_count = Random.Range(1, 3);
+                }
+                else if (player_control.playTime / 15 >= 2.0f && player_control.playTime / 15 < 4.0f) // 15초 ~ 29초일 때
+                {
+                    current.block_type = Block.TYPE.HOLE;
+                    current.max_count = Random.Range(2, 4);
+                }
+                else if (player_control.playTime / 15 >= 4.0f && player_control.playTime / 15 < 5.0f) // 60초 ~ 74초일 때
+                {
+                    current.block_type = Block.TYPE.HOLE;
+                    current.max_count = Random.Range(1, 3);
+                }
 
-            case Block.TYPE.HOLE:
-                current.block_type = Block.TYPE.FLOOR;
-                current.max_count = 10;
+                /*
+                else if (player_control.playTime / 15 >= 4.0f && player_control.playTime / 15 < 5.0f) // 60초 ~ 74초일 때
+                {
+                    current.max_count = Random.Range(1, 3); // 발판 길이 9~10개 중 랜덤 생성
+                    current.block_type = Block.TYPE.HOLE;
+                    current.height = previous.height;
+                }
+                */
+                /* else if (player_control.playTime / 15 >= 5.0f) // 75초 ~ 일 때 처음부터 15초마다 반복.
+                 {
+                     player_control.playTime = 0.0f; // 플레이 시간 0초로 초기화
+                 }*/
+                break;
+                /*
+                if (player_control.playTime / 15 == 0.0f || player_control.playTime / 15 == 1.0f || player_control.playTime / 15 == 4.0f)
+                {
+                    current.max_count = Random.Range(1, 3); // 발판 길이 9~10개 중 랜덤 생성
+                    current.block_type = Block.TYPE.HOLE;
+                    current.height = previous.height;
+           
+                }
+                if (player_control.playTime / 15 == 2.0f || player_control.playTime / 15 == 3.0f)
+                {
+                    current.max_count = Random.Range(2, 4); // 발판 길이 8~9개 중 랜덤 생성
+                    current.block_type = Block.TYPE.HOLE;
+                    current.height = previous.height;
+                }
+                break;
+                */
+
+            case Block.TYPE.HOLE: // 구멍
+                
+                if (player_control.playTime > 75.0f) // 75초 ~ 일 때 처음부터 15초마다 반복.
+                {
+                    player_control.playTime = 0.0f; // 플레이 시간 0초로 초기화
+                    player_control.playTime += Time.deltaTime;
+                }
+                
+
+                if (player_control.playTime / 15 >= 0.0f && player_control.playTime / 15 < 1.0f ) // playtime이 0초~14초일 때
+                {
+                    current.block_type = Block.TYPE.FLOOR;
+                    current.max_count = Random.Range(9, 11);
+                }
+                else if (player_control.playTime / 15 >= 1.0f && player_control.playTime / 15 < 2.0f) // 15초 ~ 29초일 때
+                {
+                    current.block_type = Block.TYPE.FLOOR;
+                    current.max_count = Random.Range(8, 10);
+                }
+                else if (player_control.playTime / 15 >= 2.0f && player_control.playTime / 15 < 3.0f) // 30초 ~ 44초일 때
+                {
+                    current.block_type = Block.TYPE.FLOOR;
+                    current.max_count = Random.Range(7, 9);
+                }
+                else if (player_control.playTime / 15 >= 3.0f && player_control.playTime / 15 < 4.0f) // 45초 ~ 59초일 때
+                {
+                    current.block_type = Block.TYPE.FLOOR;
+                    current.max_count = Random.Range(6, 8);
+                }
+                else if (player_control.playTime / 15 >= 4.0f && player_control.playTime / 15 < 5.0f) // 60초 ~ 74초일 때
+                {
+                    current.block_type = Block.TYPE.FLOOR;
+                    current.max_count = Random.Range(5, 7);
+                }
+
+                // 시간에 따른 블럭의 높이 변경
+                if (player_control.playTime / 15 >= 0.0f && player_control.playTime / 15 < 1.0f) // playtime이 0초~14초일 때
+                {
+                    current.height = 0;
+                }
+                else if (player_control.playTime / 15 >= 1.0f && player_control.playTime / 15 < 2.0f) // 15초 ~ 29초일 때
+                {
+                    current.height = Random.Range(-1, 2);
+                }
+                else if (player_control.playTime / 15 >= 2.0f && player_control.playTime / 15 < 4.0f) // 30초 ~ 44초일 때
+                {
+                    current.height = Random.Range(-2, 3) ;
+                }
+              
+                else if (player_control.playTime / 15 >= 4.0f && player_control.playTime / 15 < 5.0f) // 60초 ~ 74초일 때
+                {
+                    current.height = Random.Range(-1, 2);
+                }
+
                 break;
         }
+ 
     }
-
     public void UpdateStatus()
     {
         current_block.current_count++;
@@ -73,16 +165,5 @@ public class level_control : MonoBehaviour
             ClearNextBlock(ref next_block);
             UpdateLevel(ref next_block, current_block);
         }
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
